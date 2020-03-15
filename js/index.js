1e3d42f9ffc981f7ce452f85a0776bd637e8fd32
Anthony Donovan <chitowncoder@Anthonys-iMac.lan>
2020-03-14 19:26:19 -0500

    I have worked on adding 2 event listeners the first on is Alert and the second on is mouse enter

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+
+
+
+
+
+//* THIS IS THE EVENT LISTENER *//
+
+window.addEventListener('load', (event) => {
+    alert('Welcome to my DOMII project, I will have 10 event listeners to make the page interactive!');
+});
+
+//* EVENT LISTENER 2 *//
+
+const busImage = document.querySelector(('.intro img'));
+busImage.addEventListener('mouseenter', () => {
+    busImage.style.transform = "scale(1.1)";
+    busImage.style.transition = " all 0.3s";
+})
