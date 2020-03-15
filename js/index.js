9cf4cdc24fdd69da7274c7f3b36867ad48c285f5
Anthony <chitowncoder@gmail.com>
2020-03-14 19:53:47 -0500

    I added mouse leave, dblclick, load and wheel listeners

--- js/index.js
+++ js/index.js
@@ -16,4 +16,31 @@ const busImage = document.querySelector(('.intro img'));
 busImage.addEventListener('mouseenter', () => {
     busImage.style.transform = "scale(1.1)";
     busImage.style.transition = " all 0.3s";
-})
+});
+
+//* EVENT LISTENER 3 *//
+
+busImage.addEventListener('mouseleave', () => {
+    busImage.style.transform = "scale(1)"
+});
+
+//*  EVENT LISTENER 4 *//
+
+const changeImage = document.querySelector('.intro img');
+changeImage.addEventListener('dblclick', () => {
+    changeImage.src = "./img/bus-fun.jpg";
+});
+
+//* EVENT LISTENER 5 *//
+
+window.addEventListener('load', (event) => {
+    console.log('this page is fully loaded')
+});
+
+//* EVENT LISTENER 6 *//
+
+const hide = document.querySelector('body');
+hide.addEventListener('wheel', () => {
+    hide.style.opacity = '0';
+    hide.style.transition = 'all 0.5s';
+});
